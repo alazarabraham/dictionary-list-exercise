8e3a90216a598dff900beffd7450e22f30e01587
Alazar Markos <alazarabraham1@gmail.com>
2019-09-08 02:23:12 -0400

    first commit

---
+++ boxing.py
@@ -0,0 +1,14 @@
+boxing = [
+    {"Name": "Floyd Mayweather",
+     "Record": "50-0(27 knockouts)" ,
+     "Nickname(s)": "Money may, all fuckin day",},{"Name" : "Gennady Golovkin" or "GGG",
+     "Record": "39-1(35 Knockouts)",
+     "Nickname(s)": "Middleweight King"},{"Name": "Errol Spence",
+     "Record": "25-0 (21 Knockouts)",
+     "Nickname": "Imma knock you out tho...",},{"Name": "Deontay Wilder",
+     "Record": "42-0(40 Knockouts)",
+     "Nickname": "I lead by faith and not by sight"}]
+
+askBox = str(input("Enter your favorite boxer's first name "))
+if askBox == "floyd":
+    print(boxing)
